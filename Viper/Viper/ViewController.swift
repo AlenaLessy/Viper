555d3a730f149af54aded9f66906012558903d6f
Алена Карпова <alenapancenko00@gmail.com>
2023-02-14 19:23:36 +0300

    init

---
+++ Viper/Viper/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Viper
+//
+//  Created by Алена Панченко on 14.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        Constants.hello
+    }
+}
+
